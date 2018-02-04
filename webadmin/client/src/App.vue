11916bf45738f595c6792cdf6c866e9de4d9cc7e
__Attsun__ <atsumi.tatsuya@gmail.com>
2018-02-04 14:48:43 +0900

    workflow execution view mock

--- webadmin/client/src/App.vue
+++ webadmin/client/src/App.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="section no-pad-bot" id="app">
+  <div id="app">
+    <nav>
+      <div class="nav-wrapper teal">
+        <a href="#" class="brand-logo">Jobnetes</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Dummy1</a></li>
+          <li><a href="#">Dummy2</a></li>
+        </ul>
+      </div>
+    </nav>
     <router-view/>
+
+    <footer class="page-footer teal">
+      <div class="footer-copyright">
+        <div class="container">
+          Copyright © 2018 Tatsuya Atsumi Rights Reserved.
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
--- webadmin/client/src/components/WorkflowExecutionList.vue
+++ webadmin/client/src/components/WorkflowExecutionList.vue
@@ -1,7 +1,36 @@
 <template>
   <div>
-    <h1>WorkflowExecution List</h1>
-    <span>{{ msg }}</span>
+    <h3>WorkflowExecution List</h3>
+    <div>
+      <table class="highlight bordered">
+        <thead>
+        <tr>
+          <th>Status</th>
+          <th>Workflow Name</th>
+          <th>Execution Name</th>
+          <th>Start</th>
+          <th>End</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+          <td>{{ msg }}</td>
+          <td>Eclair</td>
+          <td>$0.87</td>
+          <td>$0.87</td>
+          <td>$0.87</td>
+        </tr>
+        <tr>
+          <td>{{ msg }}</td>
+          <td>Eclair</td>
+          <td>$0.87</td>
+          <td>$0.87</td>
+          <td>$0.87</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 

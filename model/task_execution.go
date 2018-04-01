f1fce1a9999638cee0c4afb4ceb33346abb45125
Attsun <atsumi.tatsuya@gmail.com>
2018-04-01 21:20:46 +0900

    add next, prevtask and elapsed msec

--- model/task_execution.go
+++ model/task_execution.go
@@ -27,11 +27,16 @@ type TaskExecution struct {
 	WorkflowExecutionID   uint               `gorm:"not null" json:"-"`
 	ParentTaskExecution   *TaskExecution     `json:"-"`
 	ParentTaskExecutionID uint               `json:"parentId"`
+	NextTaskExecution     *TaskExecution     `json:"-"`
+	NextTaskExecutionID   uint               `json:"nextId"`
+	PrevTaskExecution     *TaskExecution     `json:"-"`
+	PrevTaskExecutionID   uint               `json:"prevId"`
 	ExecutionName         string             `gorm:"not null" json:"executionName"`
 	TaskName              string             `gorm:"not null" json:"taskName"`
 	TaskType              string             `gorm:"not null" json:"taskType"`
 	StartedAt             *time.Time         `gorm:"not null" json:"startedAt"`
 	EndedAt               *time.Time         `json:"endedAt"`
+	ElapsedMs             uint               `json:"elapsedMs"`
 	Status                TaskStatusType     `gorm:"not null" json:"status"`
 	Input                 string             `gorm:"type:json;not null" json:"input"`
 	Output                string             `gorm:"type:json;not null" json:"output"`
